fdd9e3a8f9f2a819f21f9042ecf4b1bff15178df
ibimina hart <ibimina.c.hart@gmail.com>
2026-04-05 14:44:57 +0100

    feat: update auth page  oauth styling

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -110,11 +110,11 @@ export default function LoginPage() {
             </div>
 
             {/* OAuth Buttons */}
-            <div className="grid grid-cols-2 gap-3">
+            <div className="grid items-center justify-center gap-3">
                 <button
                     type="button"
                     onClick={() => handleOAuthLogin("google")}
-                    className="group relative flex items-center justify-center gap-3 py-3.5 px-4 rounded-2xl bg-surface-container-lowest hover:bg-white border border-transparent hover:border-outline-variant/30 transition-all duration-300 soft-touch text-on-background font-headline font-semibold overflow-hidden"
+                    className="group relative w-max flex items-center justify-center gap-3 py-3.5 px-4 rounded-2xl bg-surface-container-lowest hover:bg-white border border-transparent hover:border-outline-variant/30 transition-all duration-300 soft-touch text-on-background font-headline font-semibold overflow-hidden"
                 >
                     <div className="absolute inset-0 bg-linear-to-r from-[#4285F4]/5 via-[#34A853]/5 to-[#EA4335]/5 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
                     <svg className="w-5 h-5 relative z-10" viewBox="0 0 24 24">
@@ -125,7 +125,7 @@ export default function LoginPage() {
                     </svg>
                     <span className="relative z-10">Google</span>
                 </button>
-                <button
+                {/* <button
                     type="button"
                     onClick={() => handleOAuthLogin("apple")}
                     className="group relative flex items-center justify-center gap-3 py-3.5 px-4 rounded-2xl bg-surface-container-lowest hover:bg-on-background border border-transparent hover:border-on-background transition-all duration-300 soft-touch text-on-background hover:text-surface font-headline font-semibold overflow-hidden"
@@ -134,7 +134,7 @@ export default function LoginPage() {
                         <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z" />
                     </svg>
                     <span className="relative z-10">Apple</span>
-                </button>
+                </button> */}
             </div>
 
             {/* Divider */}
